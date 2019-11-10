e250ff7e4a4262299cd4adb36856a5837fe49a05
Marcel <mrodrigonz@gmail.com>
2019-11-09 22:24:00 -0600

    Added Car Overview Screen.

---
+++ lib/models/car.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+class Car {
+  final String id;
+  final String maker;
+  final String model;
+  final String description;
+  final String imageUrl;
+  bool isDriving;
+
+  Car({
+    @required this.id,
+    @required this.maker,
+    @required this.model,
+    @required this.description,
+    @required this.imageUrl,
+    this.isDriving = false,
+  });
+}
---
+++ lib/widgets/car_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CarItem extends StatelessWidget {
+  final String id;
+  final String maker;
+  final String model;
+  final String imageUrl;
+
+  CarItem(this.id, this.maker, this.model, this.imageUrl);
+  
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: GridTile(
+        header: GridTileBar(
+          leading: IconButton(
+            icon: Icon(
+                Icons.more_vert
+            ),
+              onPressed: () {},
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            maker,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 15.0
+            ),
+          ),
+
+          subtitle: Text(
+            id,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
